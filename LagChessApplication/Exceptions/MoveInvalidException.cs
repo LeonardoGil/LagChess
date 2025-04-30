6724b8ba803e9260c0f09a9cd1939a373988f2f8
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2025-04-30 06:34:31 -0300

    chore: Melhoria na implementação de MoveInvalidException

--- LagChessApplication/Exceptions/MoveInvalidException.cs
+++ LagChessApplication/Exceptions/MoveInvalidException.cs
@@ -1,16 +1,17 @@
-﻿using LagChessApplication.Interfaces;
+﻿using LagChessApplication.Domains;
+using LagChessApplication.Interfaces;
 using System.Drawing;
 
 namespace LagChessApplication.Exceptions
 {
-    public class MoveInvalidException(IPiece piece, Point to) : Exception
+    public class MoveInvalidException(IPiece piece, 
+                                      Point to, 
+                                      string? message = null, 
+                                      Exception? inner = null) : Exception(message ?? $"Invalid move for piece {piece.Type} to position {(Square)to}.", inner)
     {
         public IPiece Piece { get; init; } = piece;
         public Point To { get; init; } = to;
 
-        public static MoveInvalidException Create(IPiece piece, Point to)
-        {
-            return new MoveInvalidException(piece, to);
-        }
+        public static MoveInvalidException Create(IPiece piece, Point to) => new (piece, to);
     }
 }
