c938654fa43b2e8dc06476329cb834eb7b49f027
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2025-05-05 07:05:23 -0300

    chore: Organização nos testes

--- LagChessApplication.Tests/Tests/PawnTest.cs
+++ LagChessApplication.Tests/Tests/PawnTest.cs
@@ -12,58 +12,54 @@ namespace LagChessApplication.Tests.Tests
         public void Pawn_ShouldBePromoted_WhenArrivingAtPromotionRow()
         {
             var gameChess = GameChessExtension.Create();
-            var board = gameChess.Board;
 
-            board.OnPawnPromotion += () => PieceTypeEnum.Queen;
+            gameChess.Board.OnPawnPromotion += () => PieceTypeEnum.Queen;
 
-            board.MovePiece(Square.B2, Square.B3);
-            board.MovePiece(Square.A7, Square.A6);
+            gameChess.Play(Square.B2, Square.B3);
+            gameChess.Play(Square.A7, Square.A6);
 
-            board.MovePiece(Square.B3, Square.B4);
-            board.MovePiece(Square.A6, Square.A5);
+            gameChess.Play(Square.B3, Square.B4);
+            gameChess.Play(Square.A6, Square.A5);
 
-            board.MovePiece(Square.B4, Square.B5);
-            board.MovePiece(Square.A5, Square.A4);
+            gameChess.Play(Square.B4, Square.B5);
+            gameChess.Play(Square.A5, Square.A4);
 
-            board.MovePiece(Square.B5, Square.B6);
-            board.MovePiece(Square.A4, Square.A3);
+            gameChess.Play(Square.B5, Square.B6);
+            gameChess.Play(Square.A4, Square.A3);
 
-            board.MovePiece(Square.B6, Square.C7);
-            board.MovePiece(Square.A8, Square.A4);
+            gameChess.Play(Square.B6, Square.C7);
+            gameChess.Play(Square.A8, Square.A4);
 
-            board.MovePiece(Square.C7, Square.B8);
-            board.MovePiece(Square.A4, Square.H4);
+            gameChess.Play(Square.C7, Square.B8);
+            gameChess.Play(Square.A4, Square.H4);
 
-            board.MovePiece(Square.B8, Square.C8);
-            board.MovePiece(Square.H4, Square.H2);
+            gameChess.Play(Square.B8, Square.C8);
+            gameChess.Play(Square.H4, Square.H2);
         }
 
         [Fact]
         public void Pawn_ShouldThrowException_WhenMovingDiagonallyWithoutCapture()
         {
             var gameChess = GameChessExtension.Create();
-            var board = gameChess.Board;
 
-            board.MovePiece(Square.B2, Square.B3);
-            board.MovePiece(Square.A7, Square.A6);
+            gameChess.Play(Square.B2, Square.B3);
+            gameChess.Play(Square.A7, Square.A6);
 
-            Assert.Throws<MoveInvalidException>(() => board.MovePiece(Square.B3, Square.C4));
+            Assert.Throws<MoveInvalidException>(() => gameChess.Play(Square.B3, Square.C4));
         }
-
 
         [Fact]
         public void Pawn_ShouldAllowDiagonalMove_WhenCapturing()
         {
             var gameChess = GameChessExtension.Create();
-            var board = gameChess.Board;
 
-            board.MovePiece(Square.B2, Square.B3);
-            board.MovePiece(Square.A7, Square.A6);
+            gameChess.Play(Square.B2, Square.B3);
+            gameChess.Play(Square.A7, Square.A6);
 
-            board.MovePiece(Square.B3, Square.B4);
-            board.MovePiece(Square.A6, Square.A5);
+            gameChess.Play(Square.B3, Square.B4);
+            gameChess.Play(Square.A6, Square.A5);
 
-            board.MovePiece(Square.B4, Square.A5);
+            gameChess.Play(Square.B4, Square.A5);
         }
     }
 }
