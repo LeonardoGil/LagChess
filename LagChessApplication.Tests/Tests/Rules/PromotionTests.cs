b90fc215f23744bdeaf32bd891aa0cd8fbe93805
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2025-10-17 11:24:44 -0300

    Correções

--- LagChessApplication.Tests/Tests/Rules/PromotionTests.cs
+++ LagChessApplication.Tests/Tests/Rules/PromotionTests.cs
@@ -8,30 +8,33 @@ namespace LagChessApplication.Tests.Tests.Rules
     public class PromotionTests
     {
         [Fact]
-        public void Pawn_ShouldBePromoted_WhenArrivingAtPromotionRow()
+        public void Pawn_ShouldBePromoted_WhenCapturingOnLastRank()
         {
-            var chessGame = GameChessExtension.Create();
+            var chessGame = GameChessExtension.Create(onPawnPromotion: () => PieceTypeEnum.Queen);
 
-            chessGame.Play(Square.B2, Square.B3);
-            chessGame.Play(Square.A7, Square.A6);
+            chessGame.Play(Square.A2, Square.A4);
+            chessGame.Play(Square.H7, Square.H6);
+            
+            chessGame.Play(Square.A4, Square.A5);
+            chessGame.Play(Square.H6, Square.H5);
+            
+            chessGame.Play(Square.A5, Square.A6);
+            chessGame.Play(Square.H5, Square.H4);
+            
+            chessGame.Play(Square.A6, Square.B7);
+            chessGame.Play(Square.H4, Square.H3);
 
-            chessGame.Play(Square.B3, Square.B4);
-            chessGame.Play(Square.A6, Square.A5);
+            chessGame.Play(Square.B7, Square.A8);
 
-            chessGame.Play(Square.B4, Square.B5);
-            chessGame.Play(Square.A5, Square.A4);
+            var lastMove = chessGame.History.LastMove;
 
-            chessGame.Play(Square.B5, Square.B6);
-            chessGame.Play(Square.A4, Square.A3);
+            Assert.NotNull(lastMove.PawnPromotion);
+            Assert.Equal(PieceTypeEnum.Queen, lastMove.PawnPromotion);
 
-            chessGame.Play(Square.B6, Square.C7);
-            chessGame.Play(Square.A8, Square.A4);
+            var promotedPiece = chessGame.Board.GetPiece(Square.A8);
 
-            chessGame.Play(Square.C7, Square.B8);
-            chessGame.Play(Square.A4, Square.H4);
-
-            chessGame.Play(Square.B8, Square.C8);
-            chessGame.Play(Square.H4, Square.H2);
+            Assert.NotNull(promotedPiece);
+            Assert.Equal(PieceTypeEnum.Queen, promotedPiece.Type);
         }
     }
 }
--- LagChessApplication/Domains/Chess/ChessGame.cs
+++ LagChessApplication/Domains/Chess/ChessGame.cs
@@ -7,17 +7,16 @@ namespace LagChessApplication.Domains.Chess
 {
     public class ChessGame
     {
-        public ChessGame(Player white, Player black, Func<PieceTypeEnum>? onPawnPromotion = null)
+        public ChessGame(Player white, Player black, Func<PieceTypeEnum> onPawnPromotion)
         {
             ArgumentNullException.ThrowIfNull(white);
             ArgumentNullException.ThrowIfNull(black);
+            ArgumentNullException.ThrowIfNull(onPawnPromotion);
 
             White = white;
             Black = black;
 
             IPiece[] pieces = [.. White.Pieces, .. Black.Pieces];
-
-            onPawnPromotion ??= () => PieceTypeEnum.Queen;
 
             Board = new(pieces, onPawnPromotion);
             History = new();
